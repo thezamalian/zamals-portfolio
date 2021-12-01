a5ae863251686c8839c85e1bfd66b926d0fc9ee7
Zamal Hossain <thezamalian@gmail.com>
2021-12-02 02:41:34 +0600

    skills

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -4,6 +4,7 @@ const Skills = () => {
     return (
         <div>
             <h2>My skills</h2>
+            <p>I am experienced in HTML/CSS, JavaScript, React, MaterialUI, Bootstrap, Node.js, Express.js and MongoDB.</p>
         </div>
     );
 };
